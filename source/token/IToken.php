012468e36bdc4a11d30a7400c91855bff6a91d6f
chkt <ck@chkt.one>
2018-12-06 17:39:19 +0800

    Add ::TOKEN_NUMBER_LITERAL, ::TOKEN_VALUE

--- source/token/IToken.php
+++ source/token/IToken.php
@@ -11,15 +11,19 @@ extends IProjectable
 {
 
 	const TOKEN_EXPRESSION = 1;
+
 	const TOKEN_EXPRESSION_GROUP = 2;
-	const TOKEN_EXPRESSION_LIST = 3;
+	const TOKEN_ACCESS_GROUP = 3;
+	const TOKEN_CALL_GROUP = 4;
 
-	const TOKEN_NAME_LITERAL = 5;
-	const TOKEN_ACCESS_GROUP = 7;
+	const TOKEN_EXPRESSION_LIST = 5;
 
-	const TOKEN_OPERATOR = 8;
-	const TOKEN_CALL_GROUP = 9;
+	const TOKEN_NUMBER_LITERAL = 6;
+	const TOKEN_NAME_LITERAL = 7;
 
+	const TOKEN_VALUE = 8;
+
+	const TOKEN_OPERATOR = 9;
 	const TOKEN_BINARY_OPERATION = 10;
 
 
