c4aa246c5ee55dcfd9cb444488cf95211937c641
chkt <ck@chkt.one>
2018-12-06 17:51:48 +0800

    Add value resolution and arithmetic operation type guards Add return type guards for _opDiv() and _opMod()

--- source/Solver.php
+++ source/Solver.php
@@ -22,6 +22,15 @@ implements ISolver
 	}
 
 
+	private function _isArithmeticArgs($a, $b) : bool {
+		$filter = [ 'boolean', 'integer', 'double' ];
+
+		return
+			in_array(gettype($a), $filter) &&
+			in_array(gettype($b), $filter);
+	}
+
+
 	private function _opAccess(array $source, $prop) {
 		if (!array_key_exists($prop, $source)) throw new \ErrorException(sprintf(
 			'EXPR inaccessible "%2$s" in [%1$s]',
@@ -37,46 +46,41 @@ implements ISolver
 	}
 
 	private function _opMul($a, $b) {
+		if (!$this->_isArithmeticArgs($a, $b)) throw new \ErrorException();
+
 		return $a * $b;
 	}
 
 	private function _opDiv($a, $b) {
-		return $a / $b;
+		if (!$this->_isArithmeticArgs($a, $b)) throw new \ErrorException();
+
+		return empty($b) ? $a / abs($a) * INF : $a / $b;
 	}
 
 	private function _opMod($a, $b) {
-		return $a % $b;
+		if (!$this->_isArithmeticArgs($a, $b)) throw new \ErrorException();
+
+		return empty($b) ? NAN : $a % $b;
 	}
 
 	private function _opAdd($a, $b) {
+		if (!$this->_isArithmeticArgs($a, $b)) throw new \ErrorException();
+
 		return $a + $b;
 	}
 
 	private function _opSub($a, $b) {
+		if (!$this->_isArithmeticArgs($a, $b)) throw new \ErrorException();
+
 		return $a - $b;
 	}
 
 
-	private function _resolveNameOrOperation(IToken $token) {
+	private function _resolveName(IToken $token) {
 		$type = $token->getType();
 
 		switch ($type) {
-			case IToken::TOKEN_NAME_LITERAL :
-				return $token->getChars();
-
-			case IToken::TOKEN_BINARY_OPERATION :
-				return $this->_resolveBinaryOperation($token);
-
-			default : throw new \ErrorException($type);
-		}
-	}
-
-	private function _resolveVariableOrOperation(IToken $token) {
-		$type = $token->getType();
-
-		switch ($type) {
-			case IToken::TOKEN_NAME_LITERAL : return $this->_opAccess($this->_context, $token->getChars());
-			case IToken::TOKEN_BINARY_OPERATION : return $this->_resolveBinaryOperation($token);
+			case IToken::TOKEN_NAME_LITERAL : return $token->getChars();
 
 			default : throw new \ErrorException($type);
 		}
@@ -87,17 +91,17 @@ implements ISolver
 		$left = $token->getOperandAt(IOperationToken::OPERAND_BINARY_BEFORE);
 		$right = $token->getOperandAt(IOperationToken::OPERAND_BINARY_AFTER);
 
-		$lval = $this->_resolveVariableOrOperation($left);
+		$lval = $this->_resolveExpression($left);
 
 		switch ($op) {
-			case '.' : return $this->_opAccess($lval, $this->_resolveNameOrOperation($right));
+			case '.' : return $this->_opAccess($lval, $this->_resolveName($right));
 			case '[...]' : return $this->_opAccess($lval, $this->_resolveExpression($right));
 			case 'call' : return $this->_opCall($lval, $this->_resolveExpressionList($right));
-			case '*' : return $this->_opMul($lval, $this->_resolveVariableOrOperation($right));
-			case '/' : return $this->_opDiv($lval, $this->_resolveVariableOrOperation($right));
-			case '%' : return $this->_opMod($lval, $this->_resolveVariableOrOperation($right));
-			case '+' : return $this->_opAdd($lval, $this->_resolveVariableOrOperation($right));
-			case '-' : return $this->_opSub($lval, $this->_resolveVariableOrOperation($right));
+			case '*' : return $this->_opMul($lval, $this->_resolveExpression($right));
+			case '/' : return $this->_opDiv($lval, $this->_resolveExpression($right));
+			case '%' : return $this->_opMod($lval, $this->_resolveExpression($right));
+			case '+' : return $this->_opAdd($lval, $this->_resolveExpression($right));
+			case '-' : return $this->_opSub($lval, $this->_resolveExpression($right));
 			default : throw new \ErrorException($op);
 		}
 	}
@@ -115,6 +119,7 @@ implements ISolver
 
 		switch ($type) {
 			case IToken::TOKEN_NAME_LITERAL : return $this->_opAccess($this->_context, $token->getChars());
+			case IToken::TOKEN_VALUE : return $token->getValue();
 			case IToken::TOKEN_BINARY_OPERATION : return $this->_resolveBinaryOperation($token);
 			default : throw new \ErrorException($type);
 		}
