9a82063a04d2f1803463334f73241742a6ddbddd
chkt <ck@chkt.one>
2019-02-23 12:30:56 +0800

    Add FloatOps

---
+++ source/api/FloatOps.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+namespace ash\api;
+
+
+
+class FloatOps
+extends AOps
+{
+
+	public function addFloat(float $a, float $b) : float {
+		return $a + $b;
+	}
+
+	public function mulFloat(float $a, float $b) : float {
+		return $a * $b;
+	}
+
+	public function subFloat(float $a, float $b) : float {
+		return $a - $b;
+	}
+
+	public function divFloat(float $a, float $b) : float {
+		if ($b !== 0.0) return $a / $b;
+
+		$sgn = (float) ($a <=> 0);
+
+		if ($sgn !== 0.0) return INF * $sgn;
+		else return NAN;
+	}
+
+	public function modFloat(float $a, float $b) : float {
+		if ($b !== 0.0) return fmod($a, $b);
+		else return NAN;
+	}
+
+	public function addInt(float $a, int $b) : float {
+		return $a + (float) $b;
+	}
+
+	public function mulInt(float $a, int $b) : float {
+		return $a * (float) $b;
+	}
+
+	public function subInt(float $a, int $b) : float {
+		return $a - (float) $b;
+	}
+
+	public function divInt(float $a, int $b) : float {
+		return $this->divFloat($a, (float) $b);
+	}
+
+	public function modInt(float $a, int $b) : float {
+		return $this->modFloat($a, (float) $b);
+	}
+}
