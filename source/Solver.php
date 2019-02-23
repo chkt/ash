bcfa5c5fcde9ff6a2b18802a19ec10ac3cbcaee0
chkt <ck@chkt.one>
2019-02-23 12:49:52 +0800

    Change Solver to use Operator Api instead of inline operator resolution

--- source/Solver.php
+++ source/Solver.php
@@ -2,6 +2,8 @@
 
 namespace ash;
 
+use eve\common\IHost;
+use ash\api\IOps;
 use ash\token\IToken;
 use ash\token\IOperationToken;
 use ash\token\IListToken;
@@ -13,74 +15,40 @@ implements ISolver
 {
 
 	private $_context;
+	private $_api;
 	private $_root;
 
 
-	public function __construct(IToken $root) {
+	public function __construct(IHost $api, IToken $root) {
 		$this->_context = null;
+		$this->_api = $api;
 		$this->_root = $root;
 	}
 
 
-	private function _isArithmeticArgs($a, $b) : bool {
-		$filter = [ 'boolean', 'integer', 'double' ];
+	private function _getType($value) : string {
+		$type = gettype($value);
 
-		return
-			in_array(gettype($a), $filter) &&
-			in_array(gettype($b), $filter);
+		switch ($type) {
+			case 'NULL' : return 'null';
+			case 'integer' : return 'int';
+			case 'double' : return 'float';
+			case 'string' : return class_exists($value) ? $value : $type;		//TODO: technically all strings representing global functions or static methods are callable...
+			case 'array' : return count($value) === 2 && is_callable($value) ? 'fn' : $type;
+			case 'object' : return is_callable($value) ? 'fn' : get_class($value);
+			default : return $type;
+		}
 	}
 
-	private function _getArithmeticError($a, $b) {
-		return new \ErrorException(sprintf(
-			'EXPR undefined for "%s", "%s"',
-			gettype($a),
-			gettype($b)
+	private function _getOps(string $ltype) : IOps {
+		$api = $this->_api;
+		$id = 'op-' . $ltype;
+
+		if ($api->hasKey($id)) return $api->getItem($id);
+		else throw new \ErrorException(sprintf(
+			'EXPR no ops "%s"',
+			$ltype
 		));
-	}
-
-
-	private function _opAccess(array $source, $prop) {
-		if (!array_key_exists($prop, $source)) throw new \ErrorException(sprintf(
-			'EXPR inaccessible "%2$s" in [%1$s]',
-			implode(', ', array_keys($source)),
-			$prop
-		));
-
-		return $source[$prop];
-	}
-
-	private function _opCall(callable $fn, array $args) {
-		return $fn(...$args);
-	}
-
-	private function _opMul($a, $b) {
-		if (!$this->_isArithmeticArgs($a, $b)) throw $this->_getArithmeticError($a, $b);
-
-		return $a * $b;
-	}
-
-	private function _opDiv($a, $b) {
-		if (!$this->_isArithmeticArgs($a, $b)) throw $this->_getArithmeticError($a, $b);
-
-		return empty($b) ? $a / abs($a) * INF : $a / $b;
-	}
-
-	private function _opMod($a, $b) {
-		if (!$this->_isArithmeticArgs($a, $b)) throw $this->_getArithmeticError($a, $b);
-
-		return empty($b) ? NAN : $a % $b;
-	}
-
-	private function _opAdd($a, $b) {
-		if (!$this->_isArithmeticArgs($a, $b)) throw $this->_getArithmeticError($a, $b);
-
-		return $a + $b;
-	}
-
-	private function _opSub($a, $b) {
-		if (!$this->_isArithmeticArgs($a, $b)) throw $this->_getArithmeticError($a, $b);
-
-		return $a - $b;
 	}
 
 
@@ -97,24 +65,34 @@ implements ISolver
 		}
 	}
 
-	private function _resolveBinaryOperation(IOperationToken $token) {
-		$op = $token->getOperator()->getChars();
-		$left = $token->getOperandAt(IOperationToken::OPERAND_BINARY_BEFORE);
-		$right = $token->getOperandAt(IOperationToken::OPERAND_BINARY_AFTER);
 
+	private function _resolveRightOperand(string $op, IToken $right) {
+		switch ($op) {
+			case 'acc' : return $this->_resolveName($right);
+			case 'run' : return $this->_resolveExpressionList($right);
+			default : return $this->_resolveExpression($right);
+		}
+	}
+
+	private function _resolveBinaryOperation(IOperationToken $token) {
+		$left = $token->getOperandAt(IOperationToken::OPERAND_BINARY_BEFORE);
 		$lval = $this->_resolveExpression($left);
 
-		switch ($op) {
-			case '.' : return $this->_opAccess($lval, $this->_resolveName($right));
-			case '[...]' : return $this->_opAccess($lval, $this->_resolveExpression($right));
-			case 'call' : return $this->_opCall($lval, $this->_resolveExpressionList($right));
-			case '*' : return $this->_opMul($lval, $this->_resolveExpression($right));
-			case '/' : return $this->_opDiv($lval, $this->_resolveExpression($right));
-			case '%' : return $this->_opMod($lval, $this->_resolveExpression($right));
-			case '+' : return $this->_opAdd($lval, $this->_resolveExpression($right));
-			case '-' : return $this->_opSub($lval, $this->_resolveExpression($right));
-			default : throw new \ErrorException($op);
-		}
+		$ops = $this->_getOps($this->_getType($lval));
+		$op = $token->getOperator()->getChars();
+
+		$right = $token->getOperandAt(IOperationToken::OPERAND_BINARY_AFTER);
+		$rval = $this->_resolveRightOperand($op, $right);
+
+		$methodName = $ops->getMethodName($op, $this->_getType($rval));
+
+		if (method_exists($ops, $methodName)) return $ops->$methodName($lval, $rval);
+		else throw new\ErrorException(sprintf(
+			'EXPR no op "%s %s %s"',
+			$op,
+			$this->_getType($lval),
+			$this->_getType($rval)
+		));
 	}
 
 	private function _resolveExpressionList(IListToken $token) : array {
@@ -129,7 +107,9 @@ implements ISolver
 		$type = $token->getType();
 
 		switch ($type) {
-			case IToken::TOKEN_NAME_LITERAL : return $this->_opAccess($this->_context, $token->getChars());
+			case IToken::TOKEN_NAME_LITERAL : return $this->_api
+				->getItem('op-array')
+				->accString($this->_context, $token->getChars());
 			case IToken::TOKEN_VALUE : return $token->getValue();
 			case IToken::TOKEN_BINARY_OPERATION : return $this->_resolveBinaryOperation($token);
 			default : throw new \ErrorException($type);
