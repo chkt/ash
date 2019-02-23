ca13dbf2c2ee28d65ffaba37fac3bf53b86f69b2
chkt <ck@chkt.one>
2019-02-23 12:54:33 +0800

    Update ParserFactory to supply an api

--- source/ParserFactory.php
+++ source/ParserFactory.php
@@ -3,6 +3,7 @@
 namespace ash;
 
 use eve\common\IFactory;
+use eve\common\factory\ISimpleFactory;
 use eve\common\factory\IBaseFactory;
 use ash\token\TokenFactory;
 
@@ -13,22 +14,28 @@ implements IFactory
 {
 
 	private $_baseFactory;
+	private $_apiFactory;
 
 
-	public function __construct(IBaseFactory $baseFactory) {
+	public function __construct(
+		IBaseFactory $baseFactory,
+		ISimpleFactory $apiFactory
+	) {
 		$this->_baseFactory = $baseFactory;
+		$this->_apiFactory = $apiFactory;
 	}
 
 
 	public function produce() : IParser {
 		$base = $this->_baseFactory;
 
+		$api = $this->_apiFactory->produce();
 		$tokens = $base->produce(TokenFactory::class, [ $base ]);
 
 		return $base->produce(Parser::class, [
 			$base->produce(Tokenizer::class, [ $tokens ]),
 			$base->produce(Normalizer::class, [ $tokens ]),
-			$base->produce(SolverFactory::class, [ $base ])
+			$base->produce(SolverFactory::class, [ $base, $api ])
 		]);
 	}
 }
