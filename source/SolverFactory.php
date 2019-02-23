89f66c781055e2066d6fcad61e756b7fd6b9bd16
chkt <ck@chkt.one>
2019-02-23 12:53:01 +0800

    Update SolverFactory to require and supply an api

--- source/SolverFactory.php
+++ source/SolverFactory.php
@@ -2,6 +2,7 @@
 
 namespace ash;
 
+use eve\common\IHost;
 use eve\common\factory\IBaseFactory;
 use ash\token\IToken;
 
@@ -12,13 +13,15 @@ implements ISolverFactory
 {
 
 	private $_baseFactory;
+	private $_api;
 
 
-	public function __construct(IBaseFactory $baseFactory) {
+	public function __construct(IBaseFactory $baseFactory, IHost $api) {
 		$this->_baseFactory = $baseFactory;
+		$this->_api = $api;
 	}
 
 	public function produce(IToken $root) : ISolver {
-		return $this->_baseFactory->produce(Solver::class, [ $root ]);
+		return $this->_baseFactory->produce(Solver::class, [ $this->_api, $root ]);
 	}
 }
