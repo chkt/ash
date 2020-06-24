6ac9bb0d38ff1f23f7112ce9f974ad8c7091c1c1
chkt <ck@chkt.one>
2020-06-24 15:47:42 +0200

    Add string equality tests

--- test/api/StringOpsTest.php
+++ test/api/StringOpsTest.php
@@ -15,6 +15,29 @@ extends TestCase
 		return new api\StringOps();
 	}
 
+	public function testTeqStringString() {
+		$api = $this->_produceApi();
+		$method = $api->getMethodName('teq', 'string');
+
+		$val = $api->$method('foo', 'bar');
+		$this->assertFalse($val);
+		$this->assertInternalType('boolean', $val);
+
+		$this->assertTrue($api->$method('foo', 'foo'));
+		$this->assertTrue($api->$method('', ''));
+	}
+
+	public function testTneStringString() {
+		$api = $this->_produceApi();
+		$method = $api->getMethodName('tne', 'string');
+
+		$val = $api->$method('foo', 'bar');
+		$this->assertTrue($val);
+		$this->assertInternalType('boolean', $val);
+
+		$this->assertFalse($api->$method('foo', 'foo'));
+		$this->assertFalse($api->$method('', ''));
+	}
 
 	public function testPinStringArray() {
 		$api = $this->_produceApi();
