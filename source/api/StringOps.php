37512b9fa5c3cc6b99e2e64bac2ddd32671ea805
chkt <ck@chkt.one>
2020-06-24 15:39:48 +0200

    Add string comparison operations

--- source/api/StringOps.php
+++ source/api/StringOps.php
@@ -8,6 +8,14 @@ final class StringOps
 extends AOps
 {
 
+	public function teqString(string $a, string $b) : bool {
+		return $a === $b;
+	}
+
+	public function tneString(string $a, string $b) : bool {
+		return $a !== $b;
+	}
+
 	public function pinArray(string $key, array $a) : bool {
 		return array_key_exists($key, $a);
 	}
